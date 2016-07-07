71c5d726f93ac73f32c1d4e2212b080b66439bb9
Merek Orlowski <morlo088@uottawa.ca>
2016-07-07 00:55:38 -0400

    Initial commit

---
+++ app/src/test/java/com/seg3525_project/pdfviewer/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.seg3525_project.pdfviewer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
