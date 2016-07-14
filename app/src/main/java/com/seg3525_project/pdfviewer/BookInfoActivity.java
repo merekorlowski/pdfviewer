1ffc7bb817a732bfff02b6ef3faabfe0250904ea
karimBH <karim.bh@hotmail.com>
2016-07-14 01:07:13 -0400

    Karim changes with the new classes

---
+++ app/src/main/java/com/seg3525_project/pdfviewer/BookInfoActivity.java
@@ -0,0 +1,18 @@
+package com.seg3525_project.pdfviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by karim on 7/13/2016.
+ */
+public class BookInfoActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_info);
+    }
+}
